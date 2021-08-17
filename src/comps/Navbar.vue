0fc69bc754875e3ee0014afe2db4d43f410da38b
Joao Meyer <1994meyer@gmail.com>
2021-08-17 20:39:16 -0300

    Simplified and removed icon comp

--- src/comps/Navbar.vue
+++ src/comps/Navbar.vue
@@ -5,14 +5,14 @@
       <img src="@/assets/logo.png" height="48">
     </a>
 
-    <a class="navbar-burger" @click="toggle">
+    <a class="navbar-burger" ref="burger" @click="toggle">
       <span></span>
       <span></span>
       <span></span>
     </a>
   </div>
 
-  <div class="navbar-menu">
+  <div class="navbar-menu" ref="menu">
     <div class="navbar-start">
       <div class="navbar-item">
         <Search />
@@ -21,13 +21,32 @@
 
     <div class="navbar-end">
       <!-- Common items -->
-      <a class="navbar-item"><Icon icon="bell" label="Notificações" /></a>
-      <a class="navbar-item"><Icon icon="envelope" label="Mensagens" /></a>
+      <a class="navbar-item">
+        <span class="icon-text">
+          <span class="icon">
+            <font-awesome-icon icon="bell" />
+          </span>
+          <span>Notificações</span>
+        </span>
+      </a>
+      <a class="navbar-item">
+        <span class="icon-text">
+          <span class="icon">
+            <font-awesome-icon icon="envelope" />
+          </span>
+          <span>Mensagens</span>
+        </span>
+      </a>
 
       <!-- User dropdown -->
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-item has-text-black">
-          <Icon icon="user" label="Conta"/>
+          <span class="icon-text">
+            <span class="icon">
+              <font-awesome-icon icon="user" />
+            </span>
+            <span>Conta</span>
+          </span>
         </a>
 
         <!-- Dropdown items -->
@@ -46,21 +65,19 @@
 </template>
 
 <script>
-import Icon from '@/comps/Icon'
 import Search from '@/comps/Search'
 
 export default {
   name: 'Navbar',
 
   components: {
-    Icon,
     Search
   },
 
   methods: {
     toggle () {
-      document.querySelector('.navbar-menu').classList.toggle('is-active')
-      document.querySelector('.navbar-burger').classList.toggle('is-active')
+      this.$refs.menu.classList.toggle('is-active')
+      this.$refs.burger.classList.toggle('is-active')
     }
   }
 }
