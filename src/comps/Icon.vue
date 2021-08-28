1f0f6d43030c9a2c3a3ff31d767ae8685b7d3d07
Joao Meyer <1994meyer@gmail.com>
2021-08-28 13:49:23 -0300

    Added color prop

--- src/comps/Icon.vue
+++ src/comps/Icon.vue
@@ -3,13 +3,13 @@
 <span v-if="label" class="icon-text">
   <span v-if="before">{{ label }}</span>
   <span class="icon">
-    <font-awesome-icon :icon="icon" />
+    <font-awesome-icon :icon="icon" :color="color" />
   </span>
   <span v-if="!before">{{ label }}</span>
 </span>
 
 <span v-else class="icon">
-  <font-awesome-icon :icon="icon" />
+  <font-awesome-icon :icon="icon" :color="color" />
 </span>
 
 </template>
@@ -29,6 +29,9 @@ export default {
     before: {
       type: Boolean,
       default: false
+    },
+    color: {
+      type: String
     }
   }
 }
