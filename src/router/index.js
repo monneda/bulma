028bfe6c4780856b4f789732ce12de23477af310
Joao Meyer <1994meyer@gmail.com>
2021-09-29 11:36:52 -0300

    Adding 404 page route

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,44 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Home from '@/views/Home'
-import Test from '@/views/Test'
-import Profile from '@/views/Profile'
-import Landing from '@/views/Landing'
-import Portfolio from '@/views/Portfolio'
-
 const routes = [
   {
     path: '/',
+    name: 'Landing',
+    component: () => import('@/views/Landing')
+  },
+  {
+    path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/Home')
   },
   {
     path: '/c/:id',
     name: 'Portfolio',
     props: true,
-    component: Portfolio
+    component: () => import('@/views/Portfolio')
   },
   {
     path: '/u/:id',
     name: 'Profile',
     props: true,
-    component: Profile
+    component: () => import('@/views/Profile')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/l',
-    name: 'Landing page',
-    component: Landing
+    component: () => import('@/views/About')
   },
   {
     path: '/t',
     name: 'Test',
-    component: Test
+    component: () => import('@/views/Test')
+  },
+  {
+    // Adapted from:
+    //  https://next.router.vuejs.org/guide/essentials/dynamic-matching.html
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('@/views/NotFound')
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container has-background-light">
+  <div class="is-flex">
+    <div>nice</div>
+    <div>nice</div>
+    <div>nice</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
