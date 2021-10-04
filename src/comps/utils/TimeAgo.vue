025d0e3b49368436b5053e5681a3dc932a39a29a
Joao Meyer <1994meyer@gmail.com>
2021-10-04 11:56:06 -0300

    Added timeago module

---
+++ src/comps/utils/TimeAgo.vue
@@ -0,0 +1,27 @@
+<template>
+<span> {{ ago }} </span>
+</template>
+
+<script>
+import { format, register } from 'timeago.js'
+import ptBR from 'timeago.js/lib/lang/pt_BR'
+
+register('pt_BR', ptBR)
+
+export default {
+  name: 'TimeAgo',
+
+  props: {
+    time: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    ago () {
+      return format(this.time, 'pt_BR')
+    }
+  }
+}
+</script>
