aba22bc17d619702dd99cc1f45dae19e4a510b33
Maurício M <mauriciombarbosa@hotmail.com>
2021-10-06 17:42:43 -0300

    adding logic to feed suggestions

--- src/comps/feed/FeedSuggestions.vue
+++ src/comps/feed/FeedSuggestions.vue
@@ -1,18 +1,20 @@
 <template>
 <article class="box">
-  <div class="has-text-centered">
-    <span class="icon is-60x60">
-      <IconUser :src="$store.state.user.profile.picture"/>
-    </span>
-    <br>
-    <span class="title is-size-6"> {{ $store.state.user.profile.name }} </span>
-    <br>
-    <span class="subtitle is-subtitle-size"> @{{ $store.state.user.profile.username }} </span>
-  </div>
+  <router-link :to="'/u/'+$store.state.user.profile.username">
+    <div class="has-text-centered">
+      <span class="icon is-60x60">
+        <IconUser :src="$store.state.user.profile.picture"/>
+      </span>
+      <br>
+      <span class="title is-size-6"> {{ $store.state.user.profile.name }} </span>
+      <br>
+      <span class="subtitle is-subtitle-size"> @{{ $store.state.user.profile.username }} </span>
+    </div>
+  </router-link>
 
   <hr class="my-3">
 
-  <div class="mb-3">
+  <div class="pb-4">
     <div class="level">
       <div class="level-left">
         <span class="level-item has-text-weight-bold is-subtitle-size has-text-gray-2">Sugestão de investidores</span>
@@ -24,31 +26,7 @@
   </div>
 
   <template v-for="user in items" :key="user">
-    <div class="level">
-      <div class="level-left">
-        <!-- Image -->
-        <div class="level-item">
-          <span class="icon is-35x35">
-            <IconUser :src="user.picture" size="sm"/>
-          </span>
-        </div>
-
-        <!-- Username -->
-        <div class="level-item">
-          <div style="line-height: 110%">
-            <span class="is-subtitle-size has-text-weight-bold">@{{user.username}}</span>
-            <p class="has-text-grey is-subtitle-size2">{{user.name}}</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Button -->
-      <div class="level-right">
-        <div class="level-item">
-          <button class="button is-primary is-light is-small">Seguir</button>
-        </div>
-      </div>
-    </div>
+    <SimpleFollowCard @follow="follow" @unfollow="unfollow" :user="user" />
   </template>
 </article>
 </template>
@@ -56,16 +34,27 @@
 <script>
 import IconUser from '../utils/IconUser'
 import client from '../../commons/client.api'
+import SimpleFollowCard from '../utils/SimpleFollowCard'
 
 export default {
-  name: 'ProfileSuggestions',
-  components: { IconUser },
+  name: 'FeedSuggestions',
+  components: { SimpleFollowCard, IconUser },
   data: () => ({
     items: []
   }),
 
   async created () {
     this.items = await client.profile.getSuggestedFriends()
+  },
+  methods: {
+    follow (user) {
+      console.log(user)
+      user.following = true
+    },
+    unfollow (user) {
+      console.log(user)
+      user.following = false
+    }
   }
 }
 </script>
@@ -84,6 +73,9 @@ export default {
 }
 .is-subtitle-size2 {
   font-size: 0.8125rem;
+}
+.has-text-gray-1 {
+  color: $gray-1;
 }
 .has-text-gray-2 {
  color: $gray-2;
---
+++ src/comps/utils/SimpleFollowCard.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="level">
+      <router-link :to="'/u/'+user.username">
+        <div class="level-left has-text-gray-1">
+          <!-- Image -->
+          <div class="level-item">
+            <span class="icon is-35x35">
+              <IconUser :src="user.picture" size="sm"/>
+            </span>
+          </div>
+
+          <!-- Username -->
+          <div class="level-item">
+            <div style="line-height: 110%">
+              <span class="is-subtitle-size has-text-weight-bold">{{user.username}}</span>
+              <p class="has-text-grey is-subtitle-size2">{{user.name}}</p>
+            </div>
+          </div>
+        </div>
+      </router-link>
+      <!-- Button -->
+      <div class="level-right">
+        <div class="level-item">
+          <button
+            class="button is-primary is-light is-small"
+            :class="{'is-active': user.following}"
+            @click="clicked()"
+          >
+            {{ user.following ? 'Seguindo' : 'Seguir' }}
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import IconUser from './IconUser'
+export default {
+  name: 'SimpleFollowCard',
+  components: {
+    IconUser
+  },
+
+  props: {
+    user: {
+      required: true,
+      type: Object
+    }
+  },
+
+  methods: {
+    follow () {
+      this.$emit('follow', this.user)
+    },
+    unfollow () {
+      this.$emit('unfollow', this.user)
+    },
+    clicked () {
+      this.user.following ? this.$emit('unfollow', this.user) : this.$emit('follow', this.user)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.is-60x60 {
+  height: 60px;
+  width: 60px;
+}
+.is-35x35 {
+  height: 35px;
+  width: 35px;
+}
+.is-subtitle-size {
+  font-size: 0.875rem;
+}
+.is-subtitle-size2 {
+  font-size: 0.8125rem;
+}
+.has-text-gray-1 {
+  color: $gray-1;
+}
+.has-text-gray-2 {
+  color: $gray-2;
+}
+.has-text-green-2 {
+  color: $green-2;
+}
+</style>
