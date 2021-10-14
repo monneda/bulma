9848be519af43ac20728a70c29a67f2249d2a32c
Joao Meyer <1994meyer@gmail.com>
2021-10-14 17:39:25 -0300

    Minor changes to button list

--- src/comps/feed/ItemButtonList.vue
+++ src/comps/feed/ItemButtonList.vue
@@ -1,46 +1,52 @@
 <template>
 <nav class="level is-mobile">
   <!-- Like -->
-  <button
-    class="level-item button is-white is-transparent px-0"
-    @click="item.like ? $emit('unreact', item) : $emit('react', item)"
-  >
-    <span class="icon-text">
-      <span
-        class="icon is-hidden-touch"
-        :class="{ 'has-text-primary': item.like }"
-      >
-        <font-awesome-icon icon="heart" size="lg" />
+  <div class="level-item">
+    <button
+      class="button is-white is-transparent is-fullwidth"
+      @click="react"
+    >
+      <span class="icon-text">
+        <span
+          class="icon is-hidden-touch"
+          :class="{ 'has-text-primary': item.like }"
+        >
+          <font-awesome-icon icon="heart" size="lg" />
+        </span>
+        <span class="has-text-dark"> Curtir </span>
       </span>
-      <span class="has-text-dark"> Curtir </span>
-    </span>
-  </button>
+    </button>
+  </div>
 
   <!-- Share -->
-  <button
-    class="level-item button is-white is-transparent px-0"
-    @click="$emit('share', item)"
-  >
-    <span class="icon-text">
-      <span class="icon is-hidden-touch">
-        <font-awesome-icon icon="share" size="lg" />
+  <div class="level-item">
+    <button
+      class="button is-white is-transparent is-fullwidth"
+      @click="$emit('share', item)"
+    >
+      <span class="icon-text">
+        <span class="icon is-hidden-touch">
+          <font-awesome-icon icon="share" size="lg" />
+        </span>
+        <span class="has-text-dark"> Compartilhar </span>
       </span>
-      <span class="has-text-dark"> Compartilhar </span>
-    </span>
-  </button>
+    </button>
+  </div>
 
   <!-- Send -->
-  <button
-    class="level-item button is-white is-transparent px-0"
-    @click="$emit('send', item)"
-  >
-    <span class="icon-text">
-      <span class="icon is-hidden-touch">
-        <font-awesome-icon icon="paper-plane" size="lg" />
+  <div class="level-item">
+    <button
+      class="button is-white is-transparent is-fullwidth"
+      @click="$emit('send', item)"
+    >
+      <span class="icon-text">
+        <span class="icon is-hidden-touch">
+          <font-awesome-icon icon="paper-plane" size="lg" />
+        </span>
+        <span class="has-text-dark"> Enviar </span>
       </span>
-      <span class="has-text-dark"> Enviar </span>
-    </span>
-  </button>
+    </button>
+  </div>
 </nav>
 </template>
 
@@ -52,6 +58,15 @@ export default {
     item: {
       type: Object,
       required: true
+    }
+  },
+
+  methods: {
+    react () {
+      if (this.item.like) {
+        return this.$emit('unreact', this.item)
+      }
+      this.$emit('react', this.item)
     }
   }
 }
