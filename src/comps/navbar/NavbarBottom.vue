bc20d4f1f41c3ff859fd724952e49b288e4a8a8f
Joao Meyer <1994meyer@gmail.com>
2021-10-15 16:25:52 -0300

    Less clutter in bottom navbar

--- src/comps/navbar/NavbarBottom.vue
+++ src/comps/navbar/NavbarBottom.vue
@@ -1,34 +1,40 @@
 <template>
-  <section class="is-hidden-desktop has-navbar-fixed-top has-navbar-fixed-bottom-touch">
+<section class="is-hidden-desktop has-navbar-fixed-bottom-touch">
   <nav
     v-if="$store.state.user.auth"
-    class="navbar is-fixed-bottom is-inline-flex is-justify-content-space-around is-align-items-center"
+    class="navbar is-fixed-bottom"
     style="padding-bottom: env(safe-area-inset-bottom)"
   >
-    <navbar-item to="/feed" icon="home" label="" class="fa-lg" />
-    <navbar-item to="/c" icon="wallet" label="" class="fa-lg" />
-    <navbar-item to="/a" icon="chart-line" label="" class="fa-lg" />
-    <navbar-item to="/explore" icon="compass" label="" class="fa-lg" />
-    <navbar-item to="/chat" icon="comment" label="" class="fa-lg" />
-    <navbar-item to="/notifications" icon="bell" label="" class="fa-lg" />
+    <div class="container is-flex is-justify-content-space-around is-align-items-center">
+      <router-link v-for="{ link, icon } of ITEMS" :key="link" :to="link" >
+        <button class="button is-white">
+          <span class="icon is-large has-text-grey-dark">
+            <font-awesome-icon :icon="icon" size="lg" />
+          </span>
+        </button>
+      </router-link>
+    </div>
   </nav>
-  </section>
+</section>
 </template>
 
 <script>
-import NavbarItem from '@/comps/navbar/NavbarItem'
+const ITEMS = [
+  { link: '/feed', icon: 'home' },
+  { link: '/c', icon: 'wallet' },
+  // { link: '/a', icon: 'chart-line' },
+  // { link: '/explore', icon: 'compass' },
+  // { link: '/chat', icon: 'comment' },
+  { link: '/notifications', icon: 'bell' }
+]
 
 export default {
   name: 'NavbarBottom',
-
-  components: {
-    NavbarItem
-  }
+  data: () => ({ ITEMS })
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
 @media screen and (max-width: 1023px) {
   .has-navbar-fixed-bottom-touch {
     padding-bottom: 60px;
