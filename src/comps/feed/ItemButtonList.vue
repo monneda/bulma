4b10b4d0787ab0552e588f9a4116b4cc295a65a2
Joao Meyer <1994meyer@gmail.com>
2021-10-22 11:04:23 -0300

    Now like button changes based on the post being liked or not

--- src/comps/feed/ItemButtonList.vue
+++ src/comps/feed/ItemButtonList.vue
@@ -13,42 +13,12 @@
         >
           <font-awesome-icon icon="heart" size="lg" />
         </span>
-        <span class="has-text-dark"> Curtir </span>
+        <span class="has-text-dark">
+          {{ item.like === true ? 'Curtido' : 'Curtir' }}
+        </span>
       </span>
     </button>
   </div>
-
-  <!-- TODO -->
-  <!-- Share -->
-  <!-- <div class="level-item">
-    <button
-      class="button is-white is-transparent is-fullwidth"
-      @click="$emit('share', item)"
-    >
-      <span class="icon-text">
-        <span class="icon is-hidden-touch">
-          <font-awesome-icon icon="share" size="lg" />
-        </span>
-        <span class="has-text-dark"> Compartilhar </span>
-      </span>
-    </button>
-  </div> -->
-
-  <!-- TODO -->
-  <!-- Send -->
-  <!-- <div class="level-item">
-    <button
-      class="button is-white is-transparent is-fullwidth"
-      @click="$emit('send', item)"
-    >
-      <span class="icon-text">
-        <span class="icon is-hidden-touch">
-          <font-awesome-icon icon="paper-plane" size="lg" />
-        </span>
-        <span class="has-text-dark"> Enviar </span>
-      </span>
-    </button>
-  </div> -->
 </nav>
 </template>
 
