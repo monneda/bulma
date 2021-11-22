3e71dadb354516c98da19627b85923e89a1ff916
Maurício M <mauriciombarbosa@hotmail.com>
2021-11-22 16:12:12 -0300

    adding notification when a user comments on an already commented post

--- src/comps/notifications/Notification.vue
+++ src/comps/notifications/Notification.vue
@@ -1,8 +1,8 @@
 <template>
 <article :class="{'notification-new': !isViewed}">
-  <a class="is-flex is-align-items-center p-1 has-text-grey-dark" @click="router">
+  <a class="is-flex is-align-items-center p-1 has-text-grey-dark is-justify-content-flex-start" @click="router">
     <!-- Picture -->
-    <router-link :to="`/u/${notification.user.username}`" class="icon is-large mx-2">
+    <router-link :to="`/u/${notification.user.username}`" class="icon is-large">
       <c-avatar :src="notification.user.picture" round />
     </router-link>
 
@@ -13,6 +13,7 @@
           <strong> @{{ notification.user.username }} </strong>
         </router-link>
         <span class="has-text-dark pl-1"> {{ message }} </span>
+        <span class="has-text-dark" v-if="text"> {{text}}</span>
       </p>
 
       <!-- Time -->
@@ -28,10 +29,11 @@
 import TimeAgo from '@/comps/utils/TimeAgo'
 
 const TEXT = {
-  USER_COMMENT_EVENT: 'comentou na sua postagem',
+  USER_COMMENT_EVENT: 'comentou na sua postagem: ',
   USER_FOLLOWED: 'te seguiu',
   USER_LIKE_EVENT: 'curtiu sua postagem',
-  USER_TAGGED_COMMENT_EVENT: 'te marcou em um comentário'
+  USER_TAGGED_COMMENT_EVENT: 'te marcou em um comentário: ',
+  USER_COMMENT_EVENT_COMMENTED: 'comentou em uma publicação que você interagiu: '
 }
 
 export default {
@@ -54,6 +56,15 @@ export default {
     },
     isViewed () {
       return this.notification.viewedAt !== null
+    },
+    text () {
+      if (this.notification.data && this.notification.data.text) {
+        return this.notification.data.text.length < 40
+          ? this.notification.data.text
+          : this.notification.data.text.substring(0, 40) + '...'
+      } else {
+        return ''
+      }
     }
   },
 
@@ -71,6 +82,9 @@ export default {
           break
         case 'USER_FOLLOWED':
           this.$router.push(`/u/${this.notification.user.username}`)
+          break
+        case 'USER_COMMENT_EVENT_COMMENTED':
+          this.$router.push(`/p/${this.notification.data.eventId}`)
           break
         default:
           this.$router.push(`/u/${this.notification.user.username}`)
