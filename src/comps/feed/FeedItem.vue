e35577ca6bcdce1b0d5d7f450416d12d88bcdba2
Joao Meyer <1994meyer@gmail.com>
2021-11-30 14:08:43 -0300

    Working for user feed

--- src/comps/feed/FeedItem.vue
+++ src/comps/feed/FeedItem.vue
@@ -1,32 +1,32 @@
 <template>
-<div v-if="data">
+<div v-if="item">
   <PostFormEditModal
-    v-if="data.data"
-    :postData="data.data"
+    v-if="item.data"
+    :postData="item.data"
     :active="modal"
     @close="modal = false"
     @save="edit"
   />
 
   <ItemHeader
-    :item="data"
+    :item="item"
     @edit="modal = true"
     @remove="remove"
   />
 
-  <ItemTextPost v-if="data.type === 'TEXT_POST_CREATED'" :post="data" />
-  <ItemUserCreate v-if="data.type === 'USER_CREATED'" :event="data" />
-  <ItemCarteraUpdate v-if="data.type === 'WALLET_ASSETS_EDIT'" :edit="data" />
-  <ItemCarteraCreate v-if="data.type === 'NEW_WALLET'" :event="data" />
+  <ItemTextPost v-if="item.type === 'TEXT_POST_CREATED'" :post="item" />
+  <ItemUserCreate v-if="item.type === 'USER_CREATED'" :event="item" />
+  <ItemCarteraUpdate v-if="item.type === 'WALLET_ASSETS_EDIT'" :edit="item" />
+  <ItemCarteraCreate v-if="item.type === 'NEW_WALLET'" :event="item" />
 
   <br>
 
-  <ItemFooter :item="data" />
+  <ItemFooter :item="item" />
 
   <hr class="my-1">
   <ItemButtonList
     class="my-0"
-    :item="data"
+    :item="item"
     @react="react"
     @unreact="unreact"
   />
@@ -36,9 +36,9 @@
 
   <!-- Comments -->
   <ItemCommentList
-    :id="data.id"
-    @created="data.commentCount++"
-    @removed="data.commentCount--"
+    :id="item.id"
+    @created="item.commentCount++"
+    @removed="item.commentCount--"
   />
 </div>
 </template>
@@ -78,47 +78,44 @@ export default {
   },
 
   props: {
-    item: {
-      type: Object,
-      required: true
-    }
+    id: { type: String, required: true }
   },
 
   data: () => ({
     modal: false,
-    data: null
+    item: null
   }),
 
   methods: {
     async remove () {
-      await client.feed.deleteEvent(this.data.id)
-      this.$emit('remove', this.data)
+      await client.feed.deleteEvent(this.item.id)
+      this.$emit('remove', this.item)
     },
 
     async edit (item) {
-      const update = { ...this.data, data: item }
-      this.data = await client.feed.editPost(update)
-      this.$emit('edit', this.data)
+      const update = { ...this.item, data: item }
+      this.item = await client.feed.editPost(update)
+      this.$emit('edit', this.item)
       this.modal = false
     },
 
     async react () {
-      await client.feed.reactEvent(this.data.id)
-      this.data.likeCount++
-      this.data.like = true
-      this.$emit('react', this.data)
+      await client.feed.reactEvent(this.item.id)
+      this.item.likeCount++
+      this.item.like = true
+      this.$emit('react', this.item)
     },
 
     async unreact () {
-      await client.feed.unreactEvent(this.data.id)
-      this.data.likeCount--
-      this.data.like = false
-      this.$emit('unreact', this.data)
+      await client.feed.unreactEvent(this.item.id)
+      this.item.likeCount--
+      this.item.like = false
+      this.$emit('unreact', this.item)
     }
   },
 
-  mounted () {
-    this.data = JSON.parse(JSON.stringify(this.item))
+  async mounted () {
+    this.item = await client.feed.fetchEvent(this.id)
   }
 }
 </script>
