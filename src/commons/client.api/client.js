1f2e2270ece63de2bf1637aa9d06286f8b3ec74a
Ramna Sidharta <ramna.thors@gmail.com>
2022-01-10 23:00:55 -0300

    Update port in client.js

--- src/commons/client.api/client.js
+++ src/commons/client.api/client.js
@@ -1,7 +1,7 @@
 
 const BASE = process.env.NODE_ENV === 'production'
   ? 'https://monneda.herokuapp.com/api' // prd
-  : 'http://localhost:8080/api' // dev
+  : 'http://localhost:8088/api' // dev
 
 export default class {
   constructor (token) {
