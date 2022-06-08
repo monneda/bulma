2d576362772b61fbb827ab2cd83ac44792c539f6
meyer1994 <meyer1994@users.noreply.github.com>
2022-06-08 23:08:46 +0000

    Deploying to gh-pages from @ thecartera/bulma@b22e367dec975fad1a5271ff8cc95c965637b64f 🚀

---
+++ precache-manifest.40995c3be4097501b11105c1486d3e43.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0a9ea788b9086b2e2a",
+    "url": "/css/app.21c69774.css"
+  },
+  {
+    "revision": "88db98903d3eac2f07b8",
+    "url": "/css/chunk-874ff704.bccbdcf7.css"
+  },
+  {
+    "revision": "f11a0091072322d76ac2",
+    "url": "/css/group-asset~group-cartera~group-feed~group-landing~group-notifications~group-profile.d0c5f25e.css"
+  },
+  {
+    "revision": "f16c6439f62a5f7b382f",
+    "url": "/css/group-cartera.66c08dd9.css"
+  },
+  {
+    "revision": "0bf9eb81807b5aed7bbf",
+    "url": "/css/group-feed~group-profile.8a093035.css"
+  },
+  {
+    "revision": "a12ba30c0a3d5ed59133",
+    "url": "/css/group-landing.502d63dc.css"
+  },
+  {
+    "revision": "ac94af0801524f36f310",
+    "url": "/css/group-notifications.c4a6436e.css"
+  },
+  {
+    "revision": "f657711146989ef14452",
+    "url": "/css/group-profile.4af44ca9.css"
+  },
+  {
+    "revision": "3af2250e9028e3a10b9f8540c5d887e9",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "ae3b373f9c143e42a094f6736784f0a1",
+    "url": "/img/assetpage.ae3b373f.png"
+  },
+  {
+    "revision": "3878c7676cec1554cf5faf73c9e84603",
+    "url": "/img/burger.3878c767.svg"
+  },
+  {
+    "revision": "9065e5fd5e3d4dc9ba157bb865c1e7d8",
+    "url": "/img/dantas.9065e5fd.png"
+  },
+  {
+    "revision": "8628540231d8f1ab9bb4908c54f3b72d",
+    "url": "/img/landing-3.86285402.png"
+  },
+  {
+    "revision": "7235f08d773534007f5326392ce5d931",
+    "url": "/img/landing-4.7235f08d.png"
+  },
+  {
+    "revision": "662ab467db010f39ef7cf2ee9980b931",
+    "url": "/img/landing-5.662ab467.png"
+  },
+  {
+    "revision": "efd1389e049e08794f2f15ef79002bd1",
+    "url": "/img/landing-bg-1.efd1389e.svg"
+  },
+  {
+    "revision": "9875ceab42aab84b943773516ed4a7df",
+    "url": "/img/landing-bg-2.9875ceab.svg"
+  },
+  {
+    "revision": "4683617f3ab527c48fb542b4aa5cca1d",
+    "url": "/img/landing-bg-3.4683617f.svg"
+  },
+  {
+    "revision": "38f5bceed47f0d2e1ce0777678641b68",
+    "url": "/img/logo-horizontal.38f5bcee.png"
+  },
+  {
+    "revision": "835cc555af11b449dc9f6fb908171bfd",
+    "url": "/img/logo.835cc555.svg"
+  },
+  {
+    "revision": "d07058eb1931b79fd083621b091c7be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b0a9ea788b9086b2e2a",
+    "url": "/js/app.161f97ce.js"
+  },
+  {
+    "revision": "ceb8e111e00da4e7cd15",
+    "url": "/js/chunk-2d0b309a.7aef6c7e.js"
+  },
+  {
+    "revision": "88db98903d3eac2f07b8",
+    "url": "/js/chunk-874ff704.2ce6ae2f.js"
+  },
+  {
+    "revision": "46be9c810bb99faf4210",
+    "url": "/js/chunk-vendors.d9c468c3.js"
+  },
+  {
+    "revision": "34631b4bce4f5ad51fd0",
+    "url": "/js/group-about.9609f950.js"
+  },
+  {
+    "revision": "67ebec843b43de85d77c",
+    "url": "/js/group-asset.89f2c145.js"
+  },
+  {
+    "revision": "8ec362b2cee92457679f",
+    "url": "/js/group-asset~group-cartera.66bbda22.js"
+  },
+  {
+    "revision": "de1c659d1a550c1d19d2",
+    "url": "/js/group-asset~group-cartera~group-debug~group-feed~group-landing~group-notifications~group-profile.1881d1c3.js"
+  },
+  {
+    "revision": "f11a0091072322d76ac2",
+    "url": "/js/group-asset~group-cartera~group-feed~group-landing~group-notifications~group-profile.90a23f3a.js"
+  },
+  {
+    "revision": "f16c6439f62a5f7b382f",
+    "url": "/js/group-cartera.a39ad848.js"
+  },
+  {
+    "revision": "d2664078ca1c539e424a",
+    "url": "/js/group-debug.f2b52ac9.js"
+  },
+  {
+    "revision": "7a073efa765b43638f70",
+    "url": "/js/group-feed.9f77ecda.js"
+  },
+  {
+    "revision": "0bf9eb81807b5aed7bbf",
+    "url": "/js/group-feed~group-profile.85ab7132.js"
+  },
+  {
+    "revision": "a12ba30c0a3d5ed59133",
+    "url": "/js/group-landing.9df1c6d8.js"
+  },
+  {
+    "revision": "ac94af0801524f36f310",
+    "url": "/js/group-notifications.bbbe5f8d.js"
+  },
+  {
+    "revision": "f657711146989ef14452",
+    "url": "/js/group-profile.57472998.js"
+  },
+  {
+    "revision": "bd8477a9c075d9d22f788ccfa8b32e82",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
